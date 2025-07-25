39a5d00dc9d718250d7f2a3fb5e5b1d7f030d195
ducnm168 <ductiny2003@gmail.com>
2025-07-26 03:30:49 +0700

    third commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 
-const Computers = () => {
+import CanvasLoader from '../Loader'
+// Canvas render kgian 3D, OrbitControls xoay phóng to nhỏ,
+// Preload để preload mô hình/tn trc khi render, useGLTF load file mô hình 3D định dạng .gltf hoặc .glb
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+        groundColor="black" />
+      <pointLight
+        intensity={4}
+        decay={0}
+      />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, - 3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Thêm listener lắng nghe sự thay đổi kích cỡ màn hình
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    // Định nghĩa hàm callback để xử lý khi mediaQuery thay đổi
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    // mediaQuery sự kiện change, nếu thay đổi thì gọi hàm callback trên,
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      // remove event khi comp unmount (f5, chuyển trang = router..)
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
